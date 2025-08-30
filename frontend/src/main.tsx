066dca54c357bf563549fa8adb010e4f11cd4ce2
Lucas Barbosa <lucas.brsa@hotmail.com>
2025-08-31 09:23:01 +1000

    refactor: replace AppIntegrated with App component in main entry point
    - Updated the main.tsx file to render the new App component instead of AppIntegrated.
    - This change aligns with recent architectural updates in the frontend structure.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { AppIntegrated } from "./AppIntegrated.tsx";
 import { ErrorBoundary } from "./components";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { App } from "./App.tsx";
 library.add(fas);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
-      <AppIntegrated />
+      <App />
     </ErrorBoundary>
   </StrictMode>,
 );
